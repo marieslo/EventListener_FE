8011ca5acb1cfcbe51678010fec72a617369b931
Дарья Сендерихина <dawo.xorowo@gmail.com>
2024-03-06 23:17:15 +0200

    modal and chakra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
-import { Box, Heading } from "@chakra-ui/layout"
+'use client'
+
+import { Box, Flex, Heading} from "@chakra-ui/layout"
 import "./main.css"
+import SignUpModal from "@/Components/SignUpModal/SignUpModal"
+import { useState } from "react";
+import { Button } from "@chakra-ui/button";
 
 export default function Welcome () {
-    return (<Box>
-      <Heading color="red">Welcome!</Heading>  
-    </Box>
-    
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const handleCloseModal = () => {
+      setIsModalOpen(false);
+    };
+
+    return (<Flex       direction="column"
+    alignItems="center"
+    justifyContent="center"
+    minHeight="100vh">
+      <Heading color="red" textAlign="center">Welcome!</Heading>
+      <Button colorScheme="red" variant="solid" onClick={handleOpenModal} mt="4">
+  Open Modal
+</Button>
+      <SignUpModal isOpen={isModalOpen} onClose={handleCloseModal} />
+    </Flex>
     )
 }
