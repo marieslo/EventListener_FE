fc8100212ad37020ca50f2217689038c485181db
Marie S <maria.slovohotova@gmail.com>
2024-03-09 23:51:58 +0200

    map refactored into Open street

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,142 +1,42 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Box, Flex, IconButton } from '@chakra-ui/react';
-import { SearchIcon, AddIcon, MinusIcon } from '@chakra-ui/icons';
-import tt, { NavigationControl, Marker } from '@tomtom-international/web-sdk-maps';
+import React, { useRef } from 'react';
+import { Box } from '@chakra-ui/react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 interface MapProps {
-  userCity: string;
   addresses: {
-    city: string;
-    street: string;
     place: string;
     lon: string;
     lat: string;
   }[];
 }
 
-const Map: React.FC<MapProps> = ({ userCity, addresses }) => {
-  const mapContainerRef = useRef<HTMLDivElement | null>(null);
-  const [searchQuery, setSearchQuery] = useState('');
-  const apiKey = process.env.NEXT_PUBLIC_TOMTOM_API_KEY;
-  const [mapInstance, setMapInstance] = useState<tt.Map | null>(null);
-  const markersRef = useRef<tt.Marker[]>([]);
-
-  useEffect(() => {
-    if (!mapContainerRef.current || !apiKey) return;
-
-    const map = tt.map({
-      key: apiKey,
-      container: mapContainerRef.current!,
-      zoom: 8,
-      center: [32.7941, 34.9896], // Haifa
-      language: 'en'
-    });
-
-    const navigationControl = new NavigationControl({
-      showZoom: true,
-      showPitch: false
-    });
-
-    map.addControl(navigationControl, 'top-left');
-
-    const events = [
-      { name: 'Technion - Israel Institute of Technology', address: 'Technion City, Haifa, Israel', coordinates: [32.7779, 35.0214] as [number, number] },
-      { name: 'Bahai Gardens', address: 'Hatzionut Avenue, Haifa, Israel', coordinates: [32.8196, 34.9887] as [number, number] },
-      { name: 'German Colony', address: 'Haifa, Israel', coordinates: [32.8156, 34.9937] as [number, number] }
-    ];
-
-    events.forEach(event => {
-      const marker = new Marker().setLngLat(event.coordinates);
-      const popup = new tt.Popup({ offset: 35 }).setHTML(`<b>${event.name}</b><br>${event.address}`);
-      marker.setPopup(popup).addTo(map);
-      markersRef.current.push(marker);
-    });
-
-    setMapInstance(map);
-
-    return () => {
-      map.remove();
-    };
-  }, [apiKey]);
-
-  const handleSearch = async () => {
-    if (!searchQuery || !apiKey || !mapInstance) return;
-
-    try {
-      const response = await fetch(
-        `https://api.tomtom.com/search/2/geocode/${searchQuery}.json?key=${apiKey}`
-      );
-      const data = await response.json();
-
-      if (data && data.results && data.results.length > 0) {
-        const { lat, lon } = data.results[0].position;
-        mapInstance.setCenter([lon, lat]).setZoom(12);
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      handleSearch();
-    }
-  };
-
-  const handleZoomIn = () => {
-    if (mapInstance) {
-      const currentZoom = mapInstance.getZoom();
-      mapInstance.setZoom(currentZoom + 1);
-    }
-  };
-
-  const handleZoomOut = () => {
-    if (mapInstance) {
-      const currentZoom = mapInstance.getZoom();
-      mapInstance.setZoom(currentZoom - 1);
-    }
-  };
+const Map: React.FC<MapProps> = ({ addresses }) => {
+  const mapContainerRef = useRef<any>(null);
 
   return (
     <Box>
-      <Flex>
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          placeholder="Search address..."
-          onKeyPress={handleKeyPress}
-          style={{ width: '380px', borderRadius: "5px", padding: '10px' }}
-        />
-        <IconButton
-          aria-label="Search"
-          colorScheme="red"
-          icon={<SearchIcon />}
-          onClick={handleSearch}
-        />
-        <IconButton
-          aria-label="Zoom In"
-          colorScheme="grey"
-          icon={<AddIcon />}
-          onClick={handleZoomIn}
-        />
-        <IconButton
-          aria-label="Zoom Out"
-          colorScheme="grey"
-          icon={<MinusIcon />}
-          onClick={handleZoomOut}
-        />
-      </Flex>
-      <Box
-        border="1px solid"
-        borderColor="gray.200"
-        color="gray.200"
-        borderRadius="md"
-        boxShadow="md"
-        position="static"
-        ref={mapContainerRef}
-        style={{ width: '380px', height: '65vh', marginRight: '100px' }}
-      />
+    <div style={{ width: '380px', height: '65vh', marginRight: '100px', borderRadius: '5px' }} ref={mapContainerRef}>
+        <MapContainer center={[32.7941, 34.9896]} zoom={8} style={{ width: '100%', height: '100%' }}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          {addresses.map(({ lat, lon, place }, index) => (
+            <Marker
+              position={[parseFloat(lat), parseFloat(lon)]}
+              icon={
+                new L.Icon({
+                  iconUrl: 'https://res.cloudinary.com/diunuo4xf/image/upload/v1710009519/pet-adoption/letter-e2_gyn0sw.png',
+                  iconSize: [30, 30],
+                  iconAnchor: [10, 10],
+                })
+              }
+              key={place}
+            >
+              <Popup>{"Event " + (index + 1)}</Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      </div>
     </Box>
   );
 };
